9c0a7ddbf03d52ba15edcaad8b974403d3644090
Alex Leoniuk <alex.brest.by@gmail.com>
2023-11-01 15:16:58 +0300

    feat: Styles add

--- app/views/layouts/header.php
+++ app/views/layouts/header.php
@@ -4,12 +4,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PTEhelp</title>
+    <link rel="stylesheet" href="/public/assets/styles/style-min.css">
+    <title>ptehelp.info</title>
 </head>
 
 <body>
     <div class="wrapper">
 
         <header class="header">
-            <h1>header</h1>
+            <a href="/index.php">
+                <h1>ptehelp.info</h1>
+            </a>
         </header>
